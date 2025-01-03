11ffffdfd527d54dcd68cbcfdf2a3300678251e4
dshparko <951008d@mail.ru>
2025-01-03 17:18:43 +0300

    feat: resolve encodeLine

--- src/encode-line.js
+++ src/encode-line.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Given a string, return its encoding version.
@@ -10,11 +10,26 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function encodeLine(str) {
+  if (str.length == 0) {
+    return "";
+  }
+  let encoded = "";
+  let count = 1;
+
+  for (let i = 1; i < str.length; i++) {
+    if (str[i] == str[i - 1]) {
+      count++;
+    } else {
+      encoded += (count > 1 ? count : "") + str[i - 1];
+      count = 1;
+    }
+  }
+  encoded += (count > 1 ? count : "") + str[str.length - 1];
+
+  return encoded;
 }
 
 module.exports = {
-  encodeLine
+  encodeLine,
 };
