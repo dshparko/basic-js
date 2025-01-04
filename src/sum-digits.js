ab3a83503ac385ef5c3c2af24953b5fab291821f
dshparko <951008d@mail.ru>
2025-01-04 14:49:06 +0300

    feat: resolve getSumOfFigits issue

--- src/sum-digits.js
+++ src/sum-digits.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Given a number, replace this number with
@@ -12,11 +12,18 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For 91, the result should be 1 (9 + 1 = 10, 1 + 0 = 1)
  *
  */
-function getSumOfDigits(/* n */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getSumOfDigits(n) {
+  let nStr = n.toString();
+  let sum = 0;
+  for (let i = 0; i < nStr.length; i++) {
+    sum += Number(nStr[i]);
+  }
+  if (sum.toString().length > 1) {
+    return getSumOfDigits(sum);
+  }
+  return sum;
 }
 
 module.exports = {
-  getSumOfDigits
+  getSumOfDigits,
 };
